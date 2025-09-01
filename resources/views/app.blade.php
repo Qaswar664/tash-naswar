32a59f506f1b9612beff82b3f836e9d6e12e157b
QaswarAhmad <qaswarwebtechahmad@gmail.com>
2025-09-01 12:28:22 +0500

    url-title-updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -57,8 +57,9 @@
         content="Premium quality Naswar online in Pakistan. Hygienically packed & delivered fast.">
     <meta name="twitter:image" content="{{ asset('images/og-naswar.jpg') }}">
 
-    <link rel="icon" href="{{ asset('images/logo.webp') }}" type="image/webp">
-
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
